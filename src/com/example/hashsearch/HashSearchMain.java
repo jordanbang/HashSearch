d41240b188a3122155100df88da15e6f12af531c
Jordan Bangia <jordan.bangia@mail.utoronto.ca>
2013-04-20 13:04:15 -0400

    Does JSON get of twitter data and builds Tweet Objects from them

--- src/com/example/hashsearch/HashSearchMain.java
+++ src/com/example/hashsearch/HashSearchMain.java
@@ -1,67 +1,104 @@
 package com.example.hashsearch;
 
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class HashSearchMain extends Activity {
-	
+	Tweet[] tweets;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		SharedPref.initSharedPref(this);
 		if (zGetVar("Search").equalsIgnoreCase("")){
-			zSetVar("Search","bieber".toLowerCase(Locale.CANADA));
+			zSetVar("Search","aer201".toLowerCase(Locale.CANADA));
 		}
 		this.setTitle("#"+zGetVar("Search"));
 		setContentView(R.layout.activity_has_search_main);
 		
+		BackGroundTask back = new BackGroundTask("", "", null);
+		JSONObject j=null;
+		try {
+			j = back.execute().get();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
 		
-//		try {
-//			URL twitter = new URL("http://search.twitter.com/search.json?q=%23"+zGetVar("Search").toLowerCase());
-//			URLConnection tc = twitter.openConnection();
-//			BufferedReader in = new BufferedReader(new InputStreamReader(tc.getInputStream()));
-//			String line;
-//			while ((line=in.readLine())!= null){
-//				JSONArray ja = new JSONArray(line);
-//				for(int i = 0; i <ja.length();i++){
-//					JSONObject jo = (JSONObject) ja.get(i);
-//					int x = 0;
-//				}
-//			}
-//		} catch (MalformedURLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (JSONException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		JSONArray ja=null;
+		try {
+			ja = j.getJSONArray("results");
+			JSONParse(ja);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
 		
-		
+	}
+	
+	//parses the JSON array of results into Twitter Objects
+	public void JSONParse(JSONArray results) throws JSONException, ParseException{
+		tweets = new Tweet[results.length()];
+		SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss ZZZZ");
+		for(int x = 0; x< results.length(); x++){
+			JSONObject current = results.getJSONObject(x);
+			tweets[x] = new Tweet();
+			tweets[x].newTweet(current.getString("text"), 
+					current.getString("from_user"), 
+					current.getString("from_user_name"), 
+					current.getString("profile_image_url"), 
+					format.parse(current.getString("created_at")));
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_has_search_main, menu);
+		//getMenuInflater().inflate(R.menu.activity_has_search_main, menu);
 		return true;
 	}
 
@@ -74,5 +111,55 @@ public class HashSearchMain extends Activity {
 	{
 	        SharedPref.setVar(idName, newValue);
 	}
+    
+    public class BackGroundTask extends AsyncTask<String, String, JSONObject>{
+    	List<NameValuePair> postparams = new ArrayList<NameValuePair>();
+    	String URL = null;
+    	String method = null;
+    	InputStream is = null;
+    	
+    	public BackGroundTask(String url, String method, List<NameValuePair> params){
+    		URL = url;
+    		postparams = params;
+    		this.method = method;
+    	}
+
+		@Override
+		protected JSONObject doInBackground(String... params) {
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			String url = "http://search.twitter.com/search.json?q=%23"+zGetVar("Search")+"&result_type=mixed";
+			HttpGet httpeg = new HttpGet(url);
+			HttpResponse httpResponse;
+			String json=null;
+			try {
+				httpResponse = httpClient.execute(httpeg);
+				HttpEntity httpentity = httpResponse.getEntity();
+				is = httpentity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"),8);
+				StringBuilder sb = new StringBuilder();
+				String line = null;
+				while((line = reader.readLine()) != null){
+					sb.append(line+"\n");
+				}
+				is.close();
+				json = sb.toString();
+			} catch (ClientProtocolException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			JSONObject jobj=null;
+			try {
+				jobj= new JSONObject(json);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return jobj;
+		}
+    }
 
 }
---
+++ src/com/example/hashsearch/Tweet.java
@@ -0,0 +1,60 @@
+package com.example.hashsearch;
+
+import java.util.Date;
+
+public class Tweet {
+	private String text;
+	private Date date;
+	private String twitter_handle;
+	private String name;
+	private String pic_url;
+	
+	public String getText(){
+		return text;
+	}
+	
+	public String getHandle(){
+		return twitter_handle;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getPic(){
+		return pic_url;
+	}
+	
+	public Date getCreated(){
+		return date;
+	}
+	
+	public void setText(String text_n){
+		text = text_n;
+	}
+	
+	public void setHandle(String handle){
+		twitter_handle = handle;
+	}
+	
+	public void setName(String name_n){
+		name = name_n;
+	}
+	
+	public void setPic(String pic){
+		pic_url = pic;
+	}
+	
+	public void setDateCreated(Date created){
+		date = created;
+	}
+	
+	public void newTweet(String text_n, String handle, String name_n, String pic, Date created){
+		setText(text_n);
+		setHandle(handle);
+		setName(name_n);
+		setPic(pic);
+		setDateCreated(created);
+	}
+	
+}
