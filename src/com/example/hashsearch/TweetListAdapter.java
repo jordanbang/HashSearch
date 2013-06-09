3f42879034e822df48afa4c88d7e3d5b884e8635
Jordan Bangia <george.doe231@gmail.com>
2013-06-09 18:01:23 -0400

    Remove unused inputs

--- src/com/example/hashsearch/TweetListAdapter.java
+++ src/com/example/hashsearch/TweetListAdapter.java
@@ -1,22 +1,11 @@
 package com.example.hashsearch;
 
-import java.io.File;
-import java.io.InputStream;
-import java.lang.ref.WeakReference;
-import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.MeasureSpec;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
